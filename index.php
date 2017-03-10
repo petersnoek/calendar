634e09d6e52baf74005369b5ef92236171724406
Peter Snoek <petersnoek@davinci.nl>
2017-03-10 11:18:34 +0100

    use printf instead of echo so I won't get crazy with all the ' and " quotes

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
         <link href="main.css" rel="stylesheet" type="text/css">
 	</head>
 	
-	<body>
+<body>
 
 
 <?php
@@ -26,15 +26,19 @@
 	// 1 create connection, check if connection works, or show error and stop
 	$connection = new mysqli('localhost','root','','calendar');
 	if( ! $connection ) {
-	    die('No connection: ' . mysqli_connect_error());
+	    die('Error: could not create connection: ' . mysqli_connect_error());
 	} 
 
 	// 2 get all birthdays from database and store them in $birthdays array
 	$sql = "SELECT * FROM birthdays ORDER BY month ASC";
 	if (!$result = $connection->query($sql)) {
-	  echo("Error description: " . mysqli_error($connection));
+	  echo("Error: could not execute query. Error description: " . mysqli_error($connection));
 	}
 	$birthdays = $result->fetch_all(MYSQLI_ASSOC);
+
+	/* pretty print debug info
+	highlight_string("<?php\n\$data =\n" . var_export($birthdays, true) . ";\n?>");
+	*/
 
 	// 3 print all birthday's; only print Monthname and Day once
 	setlocale(LC_ALL, 'nld_nld');
@@ -53,10 +57,17 @@
 
 		$id =  $birthday['id'];
 
-		echo '<p>
-		<a href="edit.php?id=' . $id . '">' . $birthday['person'] . ' (' . $birthday['year'] . ')</a>
-		<a href="delete.php?id=' . $id . '">x</a></p>';
+		//echo '<p>
+		//<a href="edit.php?id=' . $id . '">' . $birthday['person'] . ' (' . $birthday['year'] . ')</a>
+		//<a href="delete.php?id=' . $id . '">x</a></p>';
+
+		// paste values into html tages with sprintf
+		printf('<p><a href="edit.php?id=%s">%s (%s)</a> <a href="delete.php?id=%s">x</a></p>', 
+			$id, $birthday['person'], $birthday['year'], $id);
 
 		$lastMonth = $birthday['month'];
 		$lastDay = $birthday['day'];
 	}
+?>
+
+<p><a href="create.php">+ Toevoegen</a></p>
