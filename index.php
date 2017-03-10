e51be6d40480059ee55bd757df0c3a6845b74e26
Peter Snoek <petersnoek@davinci.nl>
2017-03-10 11:34:11 +0100

    cleaner HTML output

--- index.php
+++ index.php
@@ -7,32 +7,26 @@
 	);
 
 ?>
-
-
 <!doctype html>
-
 <html>
-	<head>
-		<title>Verjaardagskalender</title>
-        <link href="main.css" rel="stylesheet" type="text/css">
-	</head>
-	
+<head>
+	<title>Verjaardagskalender</title>
+    <link href="main.css" rel="stylesheet" type="text/css">
+</head>
 <body>
-
 
 <?php
 
-
-	// 1 create connection, check if connection works, or show error and stop
+	// create connection, check if connection works, or show error and stop
 	$connection = new mysqli('localhost','root','','calendar');
 	if( ! $connection ) {
 	    die('Error: could not create connection: ' . mysqli_connect_error());
 	} 
 
-	// 2 get all birthdays from database and store them in $birthdays array
+	// get all birthdays from database and store them in $birthdays array
 	$sql = "SELECT * FROM birthdays ORDER BY month ASC";
 	if (!$result = $connection->query($sql)) {
-	  echo("Error: could not execute query. Error description: " . mysqli_error($connection));
+	  	die("Error: could not execute query. Error description: " . mysqli_error($connection));
 	}
 	$birthdays = $result->fetch_all(MYSQLI_ASSOC);
 
@@ -40,29 +34,23 @@
 	highlight_string("<?php\n\$data =\n" . var_export($birthdays, true) . ";\n?>");
 	*/
 
-	// 3 print all birthday's; only print Monthname and Day once
+	// print all birthday's; only print Monthname and Day once
 	setlocale(LC_ALL, 'nld_nld');
 	$lastMonth = '';
 	$lastDay = '';
 	foreach($birthdays as $birthday){
 		if ($birthday['month'] != $lastMonth) {
-			echo '<h1>';
-			echo $monthNames[$birthday['month']]; 
-			echo '</h1>';
+			printf(PHP_EOL . '<h1>%s</h1>' . PHP_EOL, $monthNames[$birthday['month']] );
 		}
 
 		if ($birthday['day'] != $lastDay) {
-			echo '<h2>' . $birthday['day'] . '</h2>';
+			echo '<h2>' . $birthday['day'] . '</h2>' . PHP_EOL;
         }
 
 		$id =  $birthday['id'];
 
-		//echo '<p>
-		//<a href="edit.php?id=' . $id . '">' . $birthday['person'] . ' (' . $birthday['year'] . ')</a>
-		//<a href="delete.php?id=' . $id . '">x</a></p>';
-
-		// paste values into html tages with sprintf
-		printf('<p><a href="edit.php?id=%s">%s (%s)</a> <a href="delete.php?id=%s">x</a></p>', 
+		// print birthday's (printf replaces %s)
+		printf('<p><a href="edit.php?id=%s">%s (%s)</a> <a href="delete.php?id=%s">x</a></p>' . PHP_EOL, 
 			$id, $birthday['person'], $birthday['year'], $id);
 
 		$lastMonth = $birthday['month'];
